f5b6ad3a26c49bccd395d1789766610d85148567
Adam Bliss <arbliss@gmail.com>
2020-05-11 10:13:47 -0500

    added process.env.port for heroku

--- index.js
+++ index.js
@@ -59,6 +59,6 @@ request(options, function(error, response, body) {
 });
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server is running on port 3000.");
 });
